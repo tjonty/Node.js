01a399085bd80453f711512d392c6208fedce422
tjonty <tejanijonty2323@gmail.com>
2021-04-27 10:00:01 +0530

    level 5 - cookie, session, passport

--- Authenticate level-wise/app.js
+++ Authenticate level-wise/app.js
@@ -5,9 +5,10 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-//bcrypt
-const bcrypt = require("bcrypt");
-const saltRounds = 12;
+// PASSPORT, COOKIE, SESSION
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,6 +18,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// SETTING UP SESSION
+app.use(session({
+    secret: process.env.secret,
+    resave:  false,
+    saveUninitialized: false
+}));
+
+// INITIALIZE PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req,res) => {
     res.render("home");
@@ -37,49 +49,55 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     try {
         
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-
-            newUser.save((err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.render("secrets");
-                }
-            })
-        });        
+        User.register({ username : req.body.username}, req.body.password, (err, user) => {
+            if(err) {
+                console.log(err);
+                res.redirect("/register");
+            } else{
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
+            }
+        });
         
     } catch (error) {
         res.send("error at register: " + error.message);
     }
 });
 
+app.get("/secrets", (req, res) => {
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else{
+        res.redirect("/login");
+    }
+});
+
 app.post("/login", (req, res) => {
     try {
-        const username = req.body.username;
-        const password = req.body.password;
-        User.findOne({email: username}, (err, data) => {
-            if(err){
-                console.log(err);
-            } else {
-                if(data){
-                    bcrypt.compare(password, data.password, function (err, result) {
-                        if (result) {
-                            res.render("secrets");
-                        } else {
-                            res.send("sorry");
-                        }
-                    });
-                }
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.login(user, function(err) {
+            if(err) {
+                console.log(err); 
+            } else{
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
             }
         })
+
     } catch (error) {
         res.send("error at register: " + error.message);
     }
+});
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 mongoose.connect(process.env.db_url, { useNewUrlParser: true, useUnifiedTopology: true});
@@ -89,7 +107,19 @@ const userSchema = new mongoose.Schema({
     password: String,
 }); 
 
+// ADD PASSPORT-LOCAL-MONGOOSE TO MONGOOSE SCHEMA AS A PLUGIN
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+// PASSPORT LOCAL CONFIGURATION
+ 
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.listen(3000, function () {
    console.log("Server Started"); 
